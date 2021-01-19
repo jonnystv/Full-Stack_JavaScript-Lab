60976a93d8b7c0531e30ec63a4479318d380b56d
jonnystv <jonathan@darefoundation.com>
2021-01-19 20:46:38 +0000

    Initial Commit

---
+++ client/src/components/SightingsGrid.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+	<div id="sightingsGrid">
+		<sighting v-for="sighting in sightings" :sighting="sighting" />
+	</div>
+</template>
+
+<script>
+import { eventBus } from "@/main.js";
+import Sighting from "./Sighting";
+
+export default {
+  name: "sightings-grid",
+  components: {
+    sighting: Sighting,
+  },
+  props: ["sightings"],
+
+  mounted() {
+  
+  },
+};
+</script>
+
+<style lang="css" scoped>
+#sightingsGrid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+h2 {
+  padding: 0;
+  margin: 0;
+}
+</style>
